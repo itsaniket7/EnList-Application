c7c4aa01e2c9a64a46299f36c00af63c9ded94ea
Tejas1140 <73573613+Tejas1140@users.noreply.github.com>
2022-10-11 19:45:22 +0530

    Design fix

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -5,8 +5,6 @@ import vaibhav from '../../assets/vaibhavi.jpg';
 import Aniket from '../../assets/aniket.jpg';
 import tejas from '../../assets/tejas.jpg';
 import bg from '../../assets/footer-bg.jpg';
-// import { Link } from 'react-router-dom';
-
 
 const about = () => {
     return (
@@ -21,13 +19,13 @@ const about = () => {
                 <img src={tejas} alt=""/>
                 <div class="name12">Tejas Vaidya </div>
                 <div class="desig12">Developer</div>
-                <div class="about12">SPIT student</div>
+                {/* <div class="about12">SPIT student</div> */}
 
                 <div class="social-links12">
                     <a href="https://www.linkedin.com/in/tejas-vaidya-71a9a01a7/"><i class="fa fa-linkedin"></i></a>
-                    <a href="#"><i class="fa fa-instagram"></i></a>
-                    <a href="#"><i class="fa fa-twitter"></i></a>
-                    <a href="#"><i class="fa fa-github"></i></a>
+                    <a href="https://www.instagram.com/tejas_4522/"><i class="fa fa-instagram"></i></a>
+                    <a href="https://twitter.com/TejasVa31694343"><i class="fa fa-twitter"></i></a>
+                    <a href="https://github.com/Tejas1140"><i class="fa fa-github"></i></a>
                 </div>
             </div>
 
@@ -35,10 +33,10 @@ const about = () => {
                 <img src={Aniket} alt=""/>
                 <div class="name12">Aniket Sawant</div>
                 <div class="desig12">Developer</div>
-                <div class="about12"> SPIT Student </div>
+                {/* <div class="about12"> SPIT Student </div> */}
 
                 <div class="social-links12">
-                    <a href="#"><i class="fa fa-linkedin"></i></a>
+                    <a href="https://www.linkedin.com/in/aniket-sawant-80b649206/"><i class="fa fa-linkedin"></i></a>
                     <a href="#"><i class="fa fa-instagram"></i></a>
                     <a href="#"><i class="fa fa-twitter"></i></a>
                     <a href="#"><i class="fa fa-github"></i></a>
@@ -49,10 +47,10 @@ const about = () => {
                 <img src={vaibhav} alt=""/>
                 <div class="name12">Vaibhav Udamale </div>
                 <div class="desig12">Designer</div>
-                <div class="about12"> SPIT student</div>
+                {/* <div class="about12"> SPIT student</div> */}
 
                 <div class="social-links12">
-                    <a href="#"><i class="fa fa-linkedin"></i></a>
+                    <a href="https://www.linkedin.com/in/vaibhav-udamale-5824b51b0/"><i class="fa fa-linkedin"></i></a>
                     <a href="#"><i class="fa fa-instagram"></i></a>
                     <a href="#"><i class="fa fa-twitter"></i></a>
                     <a href="#"><i class="fa fa-github"></i></a>
